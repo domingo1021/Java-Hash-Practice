6c61a01c293638cc8f3add5db5db9fcdd253a5c2
曹鈞翔 <caojunxiang@caojunxiangdeMacBook-Pro.local>
2022-06-04 16:36:41 +0800

    add HashMap

--- src/main/java/org/example/ProductHashMap.java
+++ src/main/java/org/example/ProductHashMap.java
@@ -1,2 +1,30 @@
-package org.example;public class ProductHashMap {
+package org.example;
+import java.util.HashMap;
+
+public class ProductHashMap {
+    private HashMap<String, Integer> product_balances;
+
+    public ProductHashMap(HashMap<String, Integer> product_balances) {
+        this.product_balances = product_balances;
+    }
+
+    public int get(String nane){
+        return this.product_balances.get(nane);
+    }
+
+    public void produce(String name, int increase_number){
+        if(this.product_balances.get(name) == null) {
+            this.product_balances.put(name, increase_number);
+        }else{
+            int previous_num = product_balances.get(name);
+            this.product_balances.put(name, previous_num + increase_number);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ProductHashTable{" +
+                "product_balances=" + product_balances +
+                '}';
+    }
 }
